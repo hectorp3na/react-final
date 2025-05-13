5c994a3dab7427664213bbee3818fb6b7e215711
Hector <hectorpena6590@gmail.com>
2025-05-12 18:51:17 -0700

    homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Home from './pages/home';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <div className='App'>
     <Nav />
     <Routes>
-      <Route />
+      <Route path='/'  element={<Home />} />
     </Routes>
       </div>
     </Router>
